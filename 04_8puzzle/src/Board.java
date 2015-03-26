2b94ca8e17b4166e0e93e212b3d8f5d917a2a359
majerv <viktor.majer1@gmail.com>
2015-03-26 17:01:35 +0100

    Implement Board data structure

--- 04_8puzzle/src/Board.java
+++ 04_8puzzle/src/Board.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Board {
+
+  private static final int EMPTY_ITEM = 0;
+
+  private final int dimension;
+  private final int[][] blocks;
+
+  private final int rowOfEmpty;
+  private final int columnOfEmpty;
 
   /**
    * Constructs a board from an N-by-N array of blocks (where blocks[i][j] = block in row i, column
@@ -7,49 +19,203 @@ public class Board {
    * @param blocks
    */
   public Board(int[][] blocks) {
+    final int width = blocks.length;
+    final int count = width * width;
 
+    if (width < 2) {
+      throw new IllegalArgumentException("The size of the puzzle is too low.");
+    }
+
+    if (count > Integer.MAX_VALUE) {
+      throw new IllegalArgumentException("The size of the puzzle is too large.");
+    }
+
+    this.dimension = width;
+    this.blocks = copyOf(blocks);
+
+    // set the indices of empty position
+    final int position = findElement(EMPTY_ITEM);
+    rowOfEmpty = position / dimension;
+    columnOfEmpty = position % dimension;
   }
 
   // board dimension N
   public int dimension() {
-    return 0;
+    return dimension;
   }
 
   // number of blocks out of place
   public int hamming() {
-    return 0;
+    int wrongItems = 0;
+
+    int position;
+    int value;
+
+    for (int i = 0; i < dimension; i++) {
+      for (int j = 0; j < dimension; j++) {
+        position = i * dimension + j + 1;
+        value = blocks[i][j];
+        if (value != EMPTY_ITEM && value != position) {
+          ++wrongItems;
+        }
+      }
+    }
+
+    return wrongItems;
   }
 
   // sum of Manhattan distances between blocks and goal
   public int manhattan() {
-    return 0;
+    int distances = 0;
+
+    int goalRow;
+    int goalColumn;
+    int value;
+    int position;
+
+    for (int i = 0; i < dimension; i++) {
+      for (int j = 0; j < dimension; j++) {
+        value = blocks[i][j];
+        position = i * dimension + j + 1;
+        if (value != EMPTY_ITEM && value != position) {
+          goalRow = (value - 1) / dimension;
+          goalColumn = (value - 1) % dimension;
+
+          distances += Math.abs(i - goalRow) + Math.abs(j - goalColumn);
+        }
+      }
+    }
+
+    return distances;
   }
 
   // is this board the goal board?
   public boolean isGoal() {
-    return false;
+    return 0 == manhattan();
   }
 
-  // a boadr that is obtained by exchanging two adjacent blocks in the same row
+  // a board that is obtained by exchanging two adjacent blocks in the same row
   public Board twin() {
-    return null;
+    final int[][] copiedBlocks = copyOf(blocks);
+
+    changeTwoBlocks(copiedBlocks);
+
+    // return twin
+    return new Board(copiedBlocks);
   }
 
   // does this board equal y?
   @Override
   public boolean equals(Object y) {
-    return false;
+    if (this == y) {
+      return true;
+    }
+
+    if (!(y instanceof Board)) {
+      return false;
+    }
+
+    final Board other = (Board) y;
+    if (dimension != other.dimension) {
+      return false;
+    }
+
+    for (int i = 0; i < dimension; i++) {
+      for (int j = 0; j < dimension; j++) {
+        if (blocks[i][j] != other.blocks[i][j]) {
+          return false;
+        }
+      }
+    }
+
+    return true;
   }
 
   // all neighboring boards
   public Iterable<Board> neighbors() {
-    return null;
+    final List<Board> neighbors = new ArrayList<>();
+
+    final int[][] copiedBlocks = copyOf(blocks);
+
+    // create neighbor on the left
+    if (columnOfEmpty > 0) {
+      changeBlocks(copiedBlocks, rowOfEmpty, columnOfEmpty, rowOfEmpty, columnOfEmpty - 1);
+      neighbors.add(new Board(copiedBlocks));
+      changeBlocks(copiedBlocks, rowOfEmpty, columnOfEmpty, rowOfEmpty, columnOfEmpty - 1);
+    }
+
+    // create neighbor on the right
+    if (columnOfEmpty < dimension - 1) {
+      changeBlocks(copiedBlocks, rowOfEmpty, columnOfEmpty, rowOfEmpty, columnOfEmpty + 1);
+      neighbors.add(new Board(copiedBlocks));
+      changeBlocks(copiedBlocks, rowOfEmpty, columnOfEmpty, rowOfEmpty, columnOfEmpty + 1);
+    }
+
+    // create neighbor above
+    if (rowOfEmpty > 0) {
+      changeBlocks(copiedBlocks, rowOfEmpty, columnOfEmpty, rowOfEmpty - 1, columnOfEmpty);
+      neighbors.add(new Board(copiedBlocks));
+      changeBlocks(copiedBlocks, rowOfEmpty, columnOfEmpty, rowOfEmpty - 1, columnOfEmpty);
+    }
+
+    // create neighbor below
+    if (rowOfEmpty < dimension - 1) {
+      changeBlocks(copiedBlocks, rowOfEmpty, columnOfEmpty, rowOfEmpty + 1, columnOfEmpty);
+      neighbors.add(new Board(copiedBlocks));
+      changeBlocks(copiedBlocks, rowOfEmpty, columnOfEmpty, rowOfEmpty + 1, columnOfEmpty);
+    }
+
+    return neighbors;
   }
 
   // string representation of this board (in the output format specified below)
   @Override
   public String toString() {
-    return null;
+    StringBuilder s = new StringBuilder();
+    s.append(dimension + "\n");
+    for (int i = 0; i < dimension; i++) {
+      for (int j = 0; j < dimension; j++) {
+        s.append(String.format("%2d ", blocks[i][j]));
+      }
+      s.append("\n");
+    }
+    return s.toString();
+  }
+
+  // changes two non-empty blocks
+  private void changeTwoBlocks(final int[][] copiedBlocks) {
+    int row = 0;
+    // if empty block is in the first row, then make the change in the 2nd
+    if (copiedBlocks[0][0] == 0 || copiedBlocks[0][1] == 0) {
+      row = 1;
+    }
+
+    int tmp;
+    tmp = copiedBlocks[row][0];
+    copiedBlocks[row][0] = copiedBlocks[row][1];
+    copiedBlocks[row][1] = tmp;
+  }
+
+  private int[][] copyOf(int[][] blocks) {
+    return Arrays.copyOf(blocks, blocks.length);
+  }
+
+  private void changeBlocks(int[][] copiedBlocks, int row, int column, int row2, int column2) {
+    int tmp = copiedBlocks[row][column];
+    copiedBlocks[row][column] = copiedBlocks[row2][column2];
+    copiedBlocks[row2][column2] = tmp;
+  }
+
+  private int findElement(int valueToFind) {
+    for (int i = 0; i < dimension; i++) {
+      for (int j = 0; j < dimension; j++) {
+        if (blocks[i][j] == valueToFind) {
+          return i * dimension + j;
+        }
+      }
+    }
+
+    throw new IllegalArgumentException("No such element on the board: " + valueToFind);
   }
 
 }
---
+++ 04_8puzzle/test/BoardTest.java
@@ -0,0 +1,104 @@
+import static com.google.common.truth.Truth.ASSERT;
+import static junitparams.JUnitParamsRunner.$;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Unit test for {@link Board}.
+ */
+@RunWith(JUnitParamsRunner.class)
+public class BoardTest {
+
+  // unit under test
+  private Board board;
+
+  @Test
+  @Parameters(method = "provideBoardItems")
+  public void boardCreation(final int[][] blocks) {
+    // given
+    board = createFrom(blocks);
+
+    // then
+    System.out.println("Board: " + board.toString());
+  }
+
+  @Test
+  @Parameters(method = "provideBoardItems")
+  public void twinCreation(final int[][] blocks) {
+    // given
+    board = createFrom(blocks);
+
+    // then
+    System.out.println("Board: " + board.toString());
+    System.out.println("Twin: " + board.twin().toString());
+  }
+
+  @Test
+  @Parameters(method = "provideBoardItemsForHammingTest")
+  public void hammingHeuristic(final int[][] blocks, final int expectedHamming) {
+    // given
+    board = createFrom(blocks);
+
+    // then
+    ASSERT.that(board.hamming()).isEqualTo(expectedHamming);
+  }
+
+  @Test
+  @Parameters(method = "provideBoardItemsForManhattanTest")
+  public void manhattanHeuristic(final int[][] blocks, final int expectedManhattan) {
+    // given
+    board = createFrom(blocks);
+
+    // then
+    ASSERT.that(board.manhattan()).isEqualTo(expectedManhattan);
+  }
+
+  @Test
+  @Parameters(method = "provideBoardItems")
+  public void neighborCalculation(final int[][] blocks) {
+    // given
+    board = createFrom(blocks);
+
+    final Iterable<Board> neighbors = board.neighbors();
+
+    // then
+    System.out.println(neighbors);
+  }
+
+  private static Board createFrom(final int[][] blocks) {
+    return new Board(blocks);
+  }
+
+  public static Object[] provideBoardItems() {
+    return $(//
+        new int[][] { {1, 2}, {3, 0}}, //
+        new int[][] { {1, 2}, {0, 3}},//
+        new int[][] { {4, 1, 3}, {0, 2, 6}, {7, 5, 8}}//
+    );
+  }
+
+  public static Object[] provideBoardItemsForHammingTest() {
+    return $(//
+        $(new int[][] { {1, 2}, {3, 0}}, 0), //
+        $(new int[][] { {1, 2}, {0, 3}}, 1),//
+        $(new int[][] { {4, 1, 3}, {0, 2, 6}, {7, 5, 8}}, 5),//
+        $(new int[][] { {2, 0, 3, 4}, {1, 10, 6, 8}, {5, 9, 7, 12}, {13, 14, 11, 15}}, 9),//
+        $(new int[][] { {2, 9, 3, 5}, {8, 11, 12, 7}, {15, 4, 0, 13}, {6, 1, 10, 14}}, 14)//
+
+    );
+  }
+
+  public static Object[] provideBoardItemsForManhattanTest() {
+    return $(//
+        $(new int[][] { {1, 2}, {3, 0}}, 0), //
+        $(new int[][] { {1, 2}, {0, 3}}, 1),//
+        $(new int[][] { {4, 1, 3}, {0, 2, 6}, {7, 5, 8}}, 5),//
+        $(new int[][] { {2, 0, 3, 4}, {1, 10, 6, 8}, {5, 9, 7, 12}, {13, 14, 11, 15}}, 9),//
+        $(new int[][] { {2, 9, 3, 5}, {8, 11, 12, 7}, {15, 4, 0, 13}, {6, 1, 10, 14}}, 38)//
+    );
+  }
+
+}
